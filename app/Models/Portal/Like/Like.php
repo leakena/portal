034effbdc85c85de90d51dc33e0001f045ec7f94
iMac-10 <imac-10@iMac-10s-iMac.local>
2017-02-23 16:25:58 +0700

    [Task] Kick start database

---
+++ app/Models/Portal/Like/Like.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Portal\Like;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    //
+}
---
+++ database/migrations/2017_02_23_085250_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('body');
+            $table->string('file');
+            $table->integer('create_uid')->unsigned();
+            $table->boolean('published')->default(false);
+
+            $table->foreign('create_uid')->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
