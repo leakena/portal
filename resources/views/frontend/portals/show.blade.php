d217b0aad902a799242cc6476703396c86de425a
Hel Mab <mabhelitc@gmail.com>
2017-02-24 16:12:35 +0700

    [TASK] Adding more post action

--- resources/views/frontend/portals/show.blade.php
+++ resources/views/frontend/portals/show.blade.php
@@ -20,42 +20,11 @@
             </div>
             <div class="col-xs-6">
                 <div class="row">
+
                     <div class="col-xs-12">
                         <div class="box box-default">
                             <div class="box-header with-border">
-                                <h3 class="box-title">What're you thinking?</h3>
-                            </div>
-                            <div class="box-body">
-                                <form method="POST" action="{{ url('/posts/store') }}" enctype="multipart/form-data"
-                                      files="true" novalidate="novalidate">
-                                    {{ csrf_field() }}
-                                    <div class="form-group">
-                                        <textarea id="body_post" class="form-control" role="8" rows="4" name="body"
-                                                  id="body"></textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="published">
-                                            <input type="checkbox" name="published" id="published" checked> Published
-                                            now
-                                        </label>
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="file" name="file" accept="image/*">
-                                    </div>
-                                    <div class="form-group">
-                                        <button type="submit" class="btn btn-primary">Publish</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-
-                    @foreach($posts as $post)
-
-                        <div class="col-xs-12">
-                            <div class="box box-default">
-                                <div class="box-header with-border">
-                                    <div class="box-tools pull-right">
+                                <div class="box-tools pull-right">
                                         <span class="dropdown">
                                             <span class="btn btn-box-tool dropdown-toggle" type="button"
                                                   id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true"
@@ -71,40 +40,35 @@
                                                 <li><a href="/posts/report/{{ $post->id }}">Report</a></li>
                                             </ul>
                                         </span>
-                                    </div>
-                                    <ul class="media-list">
-                                        <li class="media">
-                                            <div class="media-left">
-                                                <img class="media-object" src="{{ asset('img/icon.png') }}"
-                                                     alt="Profile picture" style="width: 60px; height: 60px;">
-                                            </div><!--media-left-->
-
-                                            <div class="media-body">
-                                                <h4 class="media-heading">
-                                                    HEL Mab<br/>
-                                                    <small>
-                                                        <span class="text-muted">Student I5 - 2016-2017</span><br/>
-                                                        Posted {{ $post->created_at->diffForHumans() }}
-                                                    </small>
-                                                </h4>
-                                            </div><!--media-body-->
-                                        </li><!--media-->
-                                    </ul><!--media-list-->
                                 </div>
-                                <div class="box-body">
-                                    <article>{!! str_limit($post->body, 50) !!} <span><a
+                                <ul class="media-list">
+                                    <li class="media">
+                                        <div class="media-left">
+                                            <img class="media-object" src="{{ asset('img/icon.png') }}"
+                                                 alt="Profile picture" style="width: 60px; height: 60px;">
+                                        </div><!--media-left-->
 
-                                                    href="/posts/show/{{ $post->id }}"> See more</a></span></article>
-                                    <br/>
-                                    <a href="/posts/show/{{ $post->id }}">
-                                        <img src="{{ asset('img/frontend/uploads/images/'.$post->file) }}"
-                                             class="img-responsive"/>
-                                    </a>
-                                </div>
+                                        <div class="media-body">
+                                            <h4 class="media-heading">
+                                                HEL Mab<br/>
+                                                <small>
+                                                    <span class="text-muted">Student I5 - 2016-2017</span><br/>
+                                                    Posted {{ $post->created_at->diffForHumans() }}
+                                                </small>
+                                            </h4>
+                                        </div><!--media-body-->
+                                    </li><!--media-->
+                                </ul><!--media-list-->
+                            </div>
+                            <div class="box-body">
+                                <article>{!! $post->body !!}</article>
+                                <br/>
+                                <img src="{{ asset('img/frontend/uploads/images/'.$post->file) }}"
+                                     class="img-responsive"/>
                             </div>
                         </div>
+                    </div>
 
-                    @endforeach
                 </div>
             </div>
 
