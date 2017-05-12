1b1ae57f46342084c58e215e3cad22ba7ec1dc2d
leakena <eutchanleakena@gmail.com>
2017-05-12 18:37:59 +0700

    [TASK] Rendering post

--- app/Http/Controllers/Frontend/Portal/PortalController.php
+++ app/Http/Controllers/Frontend/Portal/PortalController.php
@@ -2,18 +2,16 @@
 
 namespace App\Http\Controllers\Frontend\Portal;
 
+use App\Http\Controllers\Controller;
 use App\Models\Access\User\Profile;
 use App\Models\Portal\Post\Post;
 use App\Models\Portal\Post\View;
-use App\Http\Controllers\Controller;
 use App\Models\Portal\Resume\Resume;
+use App\Repositories\Backend\User\UserContract;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\File;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Response;
-use Illuminate\Support\Facades\Storage;
-use App\Repositories\Backend\User\UserContract;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 
@@ -27,7 +25,8 @@ class PortalController extends Controller
     protected $studentPrefix;
     protected $controller;
     protected $users;
-    public function __construct( Controller $cont, UserContract $userRepo)
+
+    public function __construct(Controller $cont, UserContract $userRepo)
     {
         $this->middleware('auth');
         $this->studentPrefix = '/api/student';
@@ -44,30 +43,26 @@ class PortalController extends Controller
     public function index()
     {
 
-        $user =auth()->user();
+        $user = auth()->user();
 
-        $studentScore = $this->controller->getElementByApi($this->studentPrefix.'/score', ['student_id_card'], [$user->email], []);
-        $years = $this->controller->getElementByApi($this->academic.'/all', [], [], []);
-
+        $studentScore = $this->controller->getElementByApi($this->studentPrefix . '/score', ['student_id_card'], [$user->email], []);
+        $years = $this->controller->getElementByApi($this->academic . '/all', [], [], []);
 
 
         $studentScore = $studentScore['course_score'];
 
 
-        foreach ($years as $year){
-            if($year == end($years))
-            $academic_year = $year ;
+        foreach ($years as $year) {
+            if ($year == end($years))
+                $academic_year = $year;
         }
 
 
-        foreach ($studentScore as $score){
-            if($score == end($studentScore)){
+        foreach ($studentScore as $score) {
+            if ($score == end($studentScore)) {
                 $scores = $score;
             }
         }
-
-
-
 
 
 //        $studentScore = $studentApi['course_score'];
@@ -81,15 +76,60 @@ class PortalController extends Controller
         ];*/
 
 //        $this->users->create($input);
+        $studentData = $this->controller->getElementByApi($this->studentPrefix . '/annual-object', ['student_id_card', 'academic_year_id'], [auth()->user()->email, $academic_year['id']], []);
+        $posts_ = Post::where('published', true)->limit(3)->get();
+
+        $array_key_post = [];
+
+        foreach ($posts_ as $post) {
+
+            $array_key_post[$post->degree_id][$post->department_id][$post->grade_id][] = $post;
+
+        }
 
 
+        if (isset($array_key_post[$studentData['degree_id']])) {
+            $level_degree = $array_key_post[$studentData['degree_id']];
+            $priority_to_post = $level_degree;
+        } else {
+            $priority_to_post = null;
+        }
+        if (isset($level_degree[$studentData['department_id']])) {
 
-        $posts = Post::latest()->limit(3)->get();
+            $level_dept = $level_degree[$studentData['department_id']];
+            $priority_to_post = $level_dept;
+
+        }
+        if (isset($level_dept[$studentData['grade_id']])) {
+            $level_grade = $level_dept[$studentData['grade_id']];
+            $priority_to_post = $level_grade;
+        }
+
+        //$priority_to_post = $level_grade; //$array_key_post[$studentData['degree_id']][$studentData['department_id']][$studentData['grade_id']];
+
+        if (!is_null($priority_to_post)) {
+            foreach ($posts_ as $post) {
+                if ($post->degree_id != $studentData['degree_id']) {
+                    if ($post->department_id != $studentData['department_id']) {
+
+                        if ($post->grade_id != $studentData['grade_id']) {
+                            $priority_to_post = array_merge($priority_to_post, [$post]);
+                        }
+                    }
+                }
+
+            }
+            $posts = $priority_to_post;
+        } else {
+            $posts = $posts_;
+        }
+
+//        $posts = Post::latest()->limit(3)->get();
         $profile = Profile::where(['user_uid' => auth()->id()])->first();
         $resume = Resume::where(['user_uid' => auth()->id()])->first();
 
 //
-        $studentData = $this->controller->getElementByApi($this->studentPrefix.'/annual-object', ['student_id_card','academic_year_id'], ['e20161226', 2017], []);
+        $studentData = $this->controller->getElementByApi($this->studentPrefix . '/annual-object', ['student_id_card', 'academic_year_id'], ['e20161226', 2017], []);
 
 //
 
@@ -108,8 +148,6 @@ class PortalController extends Controller
 //        }
 
 
-
-
         //$studentData = $this->controller->getElementByApi($this->studentPrefix.'/score', ['student_annual_id', 'semester_id'], [20486, null], []);
 
         //$studentData = $studentData['data'];
@@ -122,25 +160,25 @@ class PortalController extends Controller
      * @param $year
      * @return mixed
      */
-    public function score($year){
-        $user =auth()->user();
+    public function score($year)
+    {
+        $user = auth()->user();
 
-        $studentScore = $this->controller->getElementByApi($this->studentPrefix.'/score', ['student_id_card', 'academic_year_id'], [$user->email, $year], []);
-        $academic_years = $this->controller->getElementByApi($this->academic.'/all', [], [], []);
-
+        $studentScore = $this->controller->getElementByApi($this->studentPrefix . '/score', ['student_id_card', 'academic_year_id'], [$user->email, $year], []);
+        $academic_years = $this->controller->getElementByApi($this->academic . '/all', [], [], []);
 
 
         $posts = Post::latest()->limit(3)->get();
         $profile = Profile::where(['user_uid' => auth()->id()])->first();
         $resume = Resume::where(['user_uid' => auth()->id()])->first();
 
-        if($studentScore == null || count($studentScore['course_score'])==0){
+        if ($studentScore == null || count($studentScore['course_score']) == 0) {
 
             return Response::json([
                 view('frontend.portals.index', compact('academic_years', 'posts', 'profile', 'resume', 'studentScore')),
                 'status' => false,
             ]);
-        }else{
+        } else {
             return Response::json([
                 view('frontend.portals.index', compact('academic_years', 'posts', 'profile', 'resume', 'studentScore')),
                 'status' => true,
@@ -166,13 +204,13 @@ class PortalController extends Controller
      */
     public function store(Request $request)
     {
-        $academic_years = $this->controller->getElementByApi($this->academic.'/all', [], [], []);
-        foreach ($academic_years as $year){
-            if($year == end($academic_years))
-                $academic_year = $year ;
+        $academic_years = $this->controller->getElementByApi($this->academic . '/all', [], [], []);
+        foreach ($academic_years as $year) {
+            if ($year == end($academic_years))
+                $academic_year = $year;
         }
 
-        $studentData = $this->controller->getElementByApi($this->studentPrefix.'/annual-object', ['student_id_card','academic_year_id'], [auth()->user()->email, $academic_year['id']], []);
+        $studentData = $this->controller->getElementByApi($this->studentPrefix . '/annual-object', ['student_id_card', 'academic_year_id'], [auth()->user()->email, $academic_year['id']], []);
 
         $newPost = new Post();
 
@@ -188,12 +226,12 @@ class PortalController extends Controller
             $filename = Input::file('file');
 
             $change = $filename->getClientOriginalExtension();
-            $newFilename = auth()->id() . str_random(8) . '.'.$change;
+            $newFilename = auth()->id() . str_random(8) . '.' . $change;
 //            $filename->move('docs', "{$newFilename}" . $change);
 
 //            Storage::disk('local')->put($newFilename,  file_get_contents($filename->getRealPath()));
 
-            if (Input::file('file')->getMimeType() == 'image/jpeg') {
+            if (Input::file('file')->getMimeType() == 'image/jpeg' || Input::file('file')->getMimeType() == 'image/png' || Input::file('file')->getMimeType() == 'image/jpg') {
 
                 $filename->move('img/frontend/uploads/images', "{$newFilename}");
             } else {
@@ -204,7 +242,7 @@ class PortalController extends Controller
 
         }
 
-        switch($request->btn_submit) {
+        switch ($request->btn_submit) {
 
             case 'Publish':
                 //action save here
@@ -257,48 +295,47 @@ class PortalController extends Controller
 
         if (!empty(Input::file())) {
             if ($post->profile) {
-                $file = explode(".",$post->file);
+                $file = explode(".", $post->file);
                 $filename = Input::file('file');
                 $change = $filename->getClientOriginalExtension();
                 $newfilename = auth()->id() . str_random(10) . '.';
 
                 if (Input::file('file')->getMimeType() == 'image/jpeg') {
 
-                    $filename->move('img/frontend/uploads/images', "{$newfilename}".$change);
+                    $filename->move('img/frontend/uploads/images', "{$newfilename}" . $change);
                 } else {
 
-                    $filename->move('docs', "{$newfilename}".$change);
+                    $filename->move('docs', "{$newfilename}" . $change);
                 }
 
                 if ($file[1] == 'jpg' || $file[1] == 'jpeg' || $file[1] == 'png') {
                     unlink(public_path('img/frontend/uploads/images/' . $post->file));
-                }else{
+                } else {
                     unlink(public_path('/docs' . $post->file));
                 }
 
                 $post->file = $newfilename . $change;
-            }else{
+            } else {
                 $filename = Input::file('file');
                 $change = $filename->getClientOriginalExtension();
                 $newfilename = auth()->id() . str_random(10) . '.';
                 if (Input::file('file')->getMimeType() == 'image/jpeg') {
 
-                    $filename->move('img/frontend/uploads/images', "{$newfilename}".$change);
+                    $filename->move('img/frontend/uploads/images', "{$newfilename}" . $change);
                 } else {
 
-                    $filename->move('docs', "{$newfilename}".$change);
+                    $filename->move('docs', "{$newfilename}" . $change);
                 }
                 $post->file = $newfilename . $change;
             }
 
-        }
-        elseif(request('name') == null){
+        } elseif (request('name') == null) {
 
-            if(isset($post->file)){
-                $file = explode(".",$post->file);
+            if (isset($post->file)) {
+                $file = explode(".", $post->file);
                 if ($file[1] == 'jpg' || $file[1] == 'jpeg' || $file[1] == 'png') {
                     unlink(public_path('img/frontend/uploads/images/' . $post->file));
-                }else{
+                } else {
                     unlink(public_path('docs/' . $post->file));
                 }
                 $post->file = null;
@@ -327,83 +364,116 @@ class PortalController extends Controller
     /**
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function post()
+    public function post(Request $request)
     {
-        $years = $this->controller->getElementByApi($this->academic.'/all', [], [], []);
+        $years = $this->controller->getElementByApi($this->academic . '/all', [], [], []);
 
-        foreach ($years as $year){
-            if($year == end($years))
-                $academic_year = $year ;
+        foreach ($years as $year) {
+            if ($year == end($years))
+                $academic_year = $year;
         }
 
-        $studentData = $this->controller->getElementByApi($this->studentPrefix.'/annual-object', ['student_id_card','academic_year_id'], [auth()->user()->email, $academic_year['id']], []);
+        $studentData = $this->controller->getElementByApi($this->studentPrefix . '/annual-object', ['student_id_card', 'academic_year_id'], [auth()->user()->email, $academic_year['id']], []);
 
-        $posts_ = Post::paginate(4);
-        $array_key_post = [];
+        $posts_ = Post::where('published', true)->paginate(3);
 
-        $index = 0;
-        foreach($posts_ as $post) {
-            $index ++;
+        $unpublished_post = Post::join('users', 'users.id', 'posts.create_uid')
+            ->where([
+                ['users.email', auth()->user()->email],
+                ['posts.published', false]
+            ])
+            ->get();
+
+        $posts = Post::where([
+
+            ['degree_id', $studentData['degree_id']],
+            ['department_id', $studentData['department_id']],
+            ['grade_id', $studentData['grade_id']],
+            ['published', true]
+
+        ])->paginate(3);
+
+        if(count($posts) == 0) {
+
+            $posts_id = collect(DB::table('posts')->where([
+
+                ['degree_id', $studentData['degree_id']],
+                ['department_id', $studentData['department_id']],
+                ['grade_id', $studentData['grade_id']],
+                ['published', true]
+
+            ])->pluck('id'))->toArray();
 
 
-            if($post->user->email != auth()->user()->email && $post->published == false){
+
+            $posts = Post::where([
+                    ['grade_id', '!=', $studentData['grade_id']],
+                    ['published', true]
+                ])->get();
+
+            dd($posts);
 
 
-                array_pull($posts_, $post);
 
-            }
+
+        }
+
+
+       /* $array_key_post = [];
+
+        foreach ($posts_ as $post) {
+
             $array_key_post[$post->degree_id][$post->department_id][$post->grade_id][] = $post;
-
         }
-        dd($posts_);
 
-
-        if(isset($array_key_post[$studentData['degree_id']])) {
+        if (isset($array_key_post[$studentData['degree_id']])) {
             $level_degree = $array_key_post[$studentData['degree_id']];
             $priority_to_post = $level_degree;
         } else {
             $priority_to_post = null;
         }
-        if(isset($level_degree[$studentData['department_id']])) {
+        if (isset($level_degree[$studentData['department_id']])) {
 
             $level_dept = $level_degree[$studentData['department_id']];
             $priority_to_post = $level_dept;
 
         }
-        if(isset($level_dept[$studentData['grade_id']])) {
+        if (isset($level_dept[$studentData['grade_id']])) {
             $level_grade = $level_dept[$studentData['grade_id']];
             $priority_to_post = $level_grade;
         }
 
-        //$priority_to_post = $level_grade; //$array_key_post[$studentData['degree_id']][$studentData['department_id']][$studentData['grade_id']];
 
-       if(!is_null($priority_to_post)) {
-           foreach($posts_ as $post) {
-               if($post->degree_id != $studentData['degree_id']) {
-                   if($post->department_id != $studentData['department_id']) {
+        if (!is_null($priority_to_post)) {
 
-                       if($post->grade_id != $studentData['grade_id']) {
-                           $priority_to_post = array_merge($priority_to_post, [$post]) ;
-                       }
-                   }
-               }
+            foreach ($posts_ as $post) {
+                if ($post->degree_id != $studentData['degree_id']) {
+                    if ($post->department_id != $studentData['department_id']) {
 
-           }
-           $posts = $priority_to_post;
-       } else {
+                        if ($post->grade_id != $studentData['grade_id']) {
+                            $priority_to_post = array_merge($priority_to_post, [$post]);
+                        }
+                    }
+                }
+
+            }
+
+            $posts = $priority_to_post;
+
+        } else {
             $posts = $posts_;
-       }
+        }*/
 
 
-
-        return view('frontend.portals.blog', compact('posts', 'posts_'));
+        return view('frontend.portals.blog', compact('posts', 'posts_', 'unpublished_post'));
     }
 
 
     /**
      * @return array
      */
-    private function selectFields() {
+    private function selectFields()
+    {
 
         $select = [
             'id',
@@ -430,8 +500,8 @@ class PortalController extends Controller
     public function delete(Post $post)
     {
 
-        if ($post->file){
-            if(str_contains($post->file, '.pdf'))
+        if ($post->file) {
+            if (str_contains($post->file, '.pdf'))
                 unlink(public_path('docs/' . $post->file));
             else
                 unlink(public_path('img/frontend/uploads/images/' . $post->file));
@@ -445,12 +515,13 @@ class PortalController extends Controller
      * @param Post $post
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function publish(Post $post){
+    public function publish(Post $post)
+    {
 
         $post->published = 1;
         $post->updated_at = Carbon::now();
 
-        if($post->save()){
+        if ($post->save()) {
             return redirect()->back();
         }
 
--- resources/views/frontend/portals/blog.blade.php
+++ resources/views/frontend/portals/blog.blade.php
@@ -38,6 +38,8 @@
                         </div>
                     </div>
 
+                    @include('frontend.portals.unpublished')
+
                     @foreach($posts as $post)
 
                         @include('frontend.portals.post')
@@ -48,6 +50,15 @@
                 <div class="row">
                     <div class="col-md-12">
                         {{ $posts_->links() }}
+
+                       {{-- <div class="col-md-12">
+                            <ul class="pagination">
+                                <li class="disabled"><span>«</span></li>
+                                <li class="active"><span>1</span></li>
+                                <li><a href="http://localhost:8000/posts?page=2">2</a></li>
+                                <li><a href="http://localhost:8000/posts?page=2" rel="next">»</a></li>
+                            </ul>
+                        </div>--}}
                     </div>
                 </div>
             </div>
