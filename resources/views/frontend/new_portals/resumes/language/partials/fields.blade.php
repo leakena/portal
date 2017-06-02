47eb38bbab9242fbaa611c0565f8841ced9485eb
Your Name <you@example.com>
2017-06-02 10:24:20 +0700

    [TASK] Validate form skill and language

--- resources/views/frontend/new_portals/resumes/language/partials/fields.blade.php
+++ resources/views/frontend/new_portals/resumes/language/partials/fields.blade.php
@@ -1,16 +1,25 @@
+<input type="hidden"
+       class="language_resume_id"
+       name="language_resume_id"
+       value="{{ $selectedLanguage->language_resume_id }}">
+<input type="hidden"
+       class="language_id"
+       name="language_id"
+       value="{{ $selectedLanguage->language_id }}">
+<input type="hidden"
+       class="proficiency1"
+       name="proficiency"
+       value="{{ $selectedLanguage->proficiency }}">
 
-<input type="hidden" class="language_resume_id" name="language_resume_id" value="{{ $selectedLanguage->language_resume_id }}">
-<input type="hidden" class="language_id" name="language_id" value="{{ $selectedLanguage->language_id }}">
-<input type="hidden" class="proficiency1" name="proficiency" value="{{ $selectedLanguage->proficiency }}">
 
-
-    <div class="circle margin-bottom-20" id="{{$selectedLanguage->language_id}}"></div>
-<h3 class="heading-xs name"><span class="language_name">{{ $selectedLanguage->name }}</span>
-        <input type="hidden" class="proficiency" value="{{ $selectedLanguage->proficiency }}">
-        @if( $selectedLanguage-> is_mother_tongue == true)
-            (Mother Tongue)
-        @endif
-    </h3>
+<div class="circle margin-bottom-20" id="{{$selectedLanguage->language_id}}"></div>
+<h3 class="heading-xs name">
+    <span class="language_name">{{ $selectedLanguage->name }}</span>
+    <input type="hidden" class="proficiency" value="{{ $selectedLanguage->proficiency }}">
+    @if( $selectedLanguage-> is_mother_tongue == true)
+        <span title="Mother Tongue" class="mark">(MT)</span>
+    @endif
+</h3>
 
 
 
